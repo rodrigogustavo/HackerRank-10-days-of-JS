0939e9beb771d85b112c1494c76e18101b1c9c2a
RodrigoGustavo <rodrigogustavo@gmail.com>
2018-09-28 22:58:02 -0400

    day 5 - hacker rank - 10 days of JS

---
+++ Day 5.2 - Arrow Function/ArrowFunction.js
@@ -0,0 +1,10 @@
+/*
+ * Modify and return the array so that all even elements are doubled and all odd elements are tripled.
+ * 
+ * Parameter(s):
+ * nums: An array of numbers.
+ */
+function modifyArray(nums) {
+    nums = nums.map((num) => (num % 2 == 0 ? num * 2 : num * 3));
+    return nums;
+}
