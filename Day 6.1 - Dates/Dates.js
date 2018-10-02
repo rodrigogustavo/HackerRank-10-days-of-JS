d72759b0012cc0a5af4baace0c8318d71f83b395
RodrigoGustavo <rodrigogustavo@gmail.com>
2018-10-01 20:59:33 -0400

    day 6 - hacker rank - 10 days of JS

---
+++ Day 6.1 - Dates/Dates.js
@@ -0,0 +1,10 @@
+// The days of the week are: "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"
+function getDayName(dateString) {
+    let dayName;
+    // Write your code here
+    var days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+    
+    dayName = days[new Date(dateString).getDay()];
+    
+    return dayName;
+}
